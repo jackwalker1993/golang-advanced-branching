1949305e2ca357d21780cdc7ef60a8106b1eb27e
jackwalker1993 <34360727+jackwalker1993@users.noreply.github.com>
2020-08-05 22:54:52 +0800

    Calculate and generate vehicle ratings

--- vehicle.go
+++ vehicle.go
@@ -1,32 +1,35 @@
 package main
 
 import (
-
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
 )
 
 //vehicle interface
-type vehicle interface{
-
+type vehicle interface {
 }
 
 //car struct
 type car struct {
-	model string
-	make string
+	model       string
+	make        string
 	typeVehicle string
 }
 
 //truct struct
 type truck struct {
-	model string
-	make string
+	model       string
+	make        string
 	typeVehicle string
 }
 
 //bike struct
 type bike struct {
 	model string
-	make string
+	make  string
 }
 
 // Values array for the feedback.json file
@@ -36,16 +39,16 @@ type Values struct {
 
 // Model array for the feedback.json file
 type Model struct {
-	Name string `json:"model"`
+	Name     string   `json:"model"`
 	Feedback []string `json:"feedback"`
 }
 
 //feedbackResult struct
-type feedbackResult struct{
-	feedbackTotal int
+type feedbackResult struct {
+	feedbackTotal    int
 	feedbackPositive int
 	feedbackNegative int
-	feedbackNeutral int
+	feedbackNeutral  int
 }
 
 var vehicleResult map[string]feedbackResult
@@ -62,7 +65,6 @@ const (
 )
 
 func init() {
-
 
 	inventory = []vehicle{
 		bike{"FTR 1200", "Indian"},
@@ -84,11 +86,10 @@ func init() {
 func main() {
 
 	// Generate ratings for the different vehicles
-	
+	generateRating()
 	// Print ratings for the different vehicles
 }
 
-/*
 func readJSONFile() Values {
 	jsonFile, err := os.Open("feedback.json")
 
@@ -104,4 +105,44 @@ func readJSONFile() Values {
 
 	return content
 }
-*/
+
+func generateRating() {
+	f := readJSONFile()
+
+	for _, v := range f.Models {
+		var vehResult feedbackResult
+		var vehRating rating
+
+		for _, msg := range v.Feedback {
+			text := strings.Split(msg, " ")
+			if len(text) >= 5 {
+				vehRating = 5.0
+				vehResult.feedbackTotal++
+
+				for _, word := range text {
+					switch s := strings.Trim(strings.ToLower(word), " ,.,!,?,\t,\n,\r"); s {
+					case "pleasure", "impressed", "wonderful", "fantastic", "splendid":
+						vehRating += extraPositive
+					case "help", "helpful", "thanks", "thank you", "happy":
+						vehRating += positive
+					case "not helpful", "sad", "angry", "improve", "annoy":
+						vehRating += negative
+					case "pathetic", "bad", "worse", "unfortunately", "agitated", "frustrated":
+						vehRating += extraNegative
+					}
+				}
+
+				switch {
+				case vehRating > 8.0:
+					vehResult.feedbackPositive++
+				case vehRating >= 4.0 && vehRating <= 8.0:
+					vehResult.feedbackNeutral++
+				case vehRating < 4.0:
+					vehResult.feedbackNegative++
+				}
+			}
+		}
+
+		vehicleResult[v.Name] = vehResult
+	}
+}
